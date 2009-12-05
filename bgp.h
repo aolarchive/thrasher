c58f6df1c1483aba1009a1926bf4f7fb339b5a1f
mthomas <mthomas@openbsd.local>
2009-12-05 07:05:46 -0500

    Adding bgp.h, (woops)

---
+++ bgp.h
@@ -0,0 +1,11 @@
+#include <sys/un.h>
+#include <arpa/inet.h>
+#include "bgpd.h"
+
+typedef struct _bgp_community {
+    uint16_t        asn;
+    uint16_t        community;
+} bgp_community_t;
+
+int thrash_bgp_connect(const char *);
+int thrash_bgp_inject(const uint32_t, const bgp_community_t *, int);
