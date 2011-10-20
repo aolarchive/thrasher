aa55a065ef20dd806dcafbd36ab04cedf468f728
Andy Wick <Andy.Wick@teamaol.com>
2011-10-19 21:11:46 -0400

    Simple html interface

--- httpd.c
+++ httpd.c
@@ -106,6 +106,40 @@ fill_http_blocks(void *key, blocked_node_t * val, struct evbuffer *buf)
     return FALSE;
 }
 
+gboolean
+fill_http_blocks_html(void *key, blocked_node_t * val, struct evbuffer *buf)
+{
+    char           blockedaddr[20] = { 0 };
+    char           triggeraddr[20] = { 0 };
+
+    strncpy(blockedaddr, inet_ntoa(*(struct in_addr *) &val->saddr), sizeof(blockedaddr) - 1);
+    strncpy(triggeraddr, inet_ntoa(*(struct in_addr *) &val->first_seen_addr), sizeof(triggeraddr) - 1);
+
+
+    evbuffer_add_printf(buf, "{ip:\"%s\", conn:\"%s\", c:%d, ",
+                        blockedaddr, triggeraddr, val->count);
+
+    if (val->timeout.ev_timeout.tv_sec == 0) {
+        evbuffer_add_printf(buf, "s:\"N/A\",");
+    } else  {
+        evbuffer_add_printf(buf, "s:%d,", event_remaining_seconds(&val->timeout));
+    }
+
+    if (val->hard_timeout.ev_timeout.tv_sec == 0) {
+        evbuffer_add_printf(buf, "h:\"N/A\",");
+    } else  {
+        evbuffer_add_printf(buf, "h:%d, ", event_remaining_seconds(&val->hard_timeout));
+    }
+
+    if (val->recent_block_timeout.ev_timeout.tv_sec == 0) {
+        evbuffer_add_printf(buf, "r:\"N/A\"},");
+    } else  {
+        evbuffer_add_printf(buf, "r:%d},", event_remaining_seconds(&val->recent_block_timeout));
+    }
+     
+    return FALSE;
+}
+
 void
 fill_current_connections(client_conn_t * conn, struct evbuffer *buf)
 {
@@ -126,6 +160,26 @@ fill_current_connections(client_conn_t * conn, struct evbuffer *buf)
     }
 }
 
+void
+fill_current_connections_html(client_conn_t * conn, struct evbuffer *buf)
+{
+    if (conn == NULL)
+        return;
+
+    evbuffer_add_printf(buf, "{ip:\"%s\", p:%d, r:%lld, cd:\"%15.15s\", ",
+                        inet_ntoa(*(struct in_addr *) &conn->conn_addr),
+                        ntohs(conn->conn_port),
+                        conn->requests,
+                        ctime(&conn->conn_time)+4);
+
+    if (conn->last_time) {
+        evbuffer_add_printf(buf, "ld:\"%15.15s\"},",
+                            ctime(&conn->last_time)+4);
+    } else {
+        evbuffer_add_printf(buf, "ld:\"N/A\"},");
+    }
+}
+
 gboolean
 fill_http_addr(void *key, qstats_t * val, struct evbuffer *buf)
 {
@@ -137,6 +191,22 @@ fill_http_addr(void *key, qstats_t * val, struct evbuffer *buf)
         evbuffer_add_printf(buf, "%-10s\n", "N/A");
     } else  {
         evbuffer_add_printf(buf, "%-10d\n", event_remaining_seconds(&val->timeout));
+    }
+
+    return FALSE;
+}
+
+gboolean
+fill_http_addr_html(void *key, qstats_t * val, struct evbuffer *buf)
+{
+    evbuffer_add_printf(buf, "{ip:\"%s\", c:%d, ",
+                        inet_ntoa(*(struct in_addr *) &val->saddr),
+                        val->connections);
+
+    if (val->timeout.ev_timeout.tv_sec == 0) {
+        evbuffer_add_printf(buf, "t:\"N/A\"},");
+    } else  {
+        evbuffer_add_printf(buf, "t:%d},", event_remaining_seconds(&val->timeout));
     }
 
     return FALSE;
@@ -163,6 +233,32 @@ fill_http_urihost(void *key, qstats_t * val, struct evbuffer *buf)
         evbuffer_add_printf(buf, "%.*s ", (int)MIN(40, strlen(colon+1)), colon+1);
         
     evbuffer_add_printf(buf, "\n");
+    return FALSE;
+}
+
+gboolean
+fill_http_urihost_html(void *key, qstats_t * val, struct evbuffer *buf)
+{
+    char *colon;
+
+    evbuffer_add_printf(buf, "{ip:\"%s\", c:%d, ",
+                        inet_ntoa(*(struct in_addr *) &val->saddr), 
+                        val->connections);
+
+    if (val->timeout.ev_timeout.tv_sec == 0) {
+        evbuffer_add_printf(buf, "t:\"N/A\",");
+    } else  {
+        evbuffer_add_printf(buf, "t:%d,", event_remaining_seconds(&val->timeout));
+    }
+
+    /* Print up to 40 chars of the uri or host */
+    /* ALW - Maybe need to escape this */
+    colon = strchr(key, ':');
+    if (colon)
+        evbuffer_add_printf(buf, "u:\"%.*s\"},", MIN(40, strlen(colon+1)), colon+1);
+    else
+        evbuffer_add_printf(buf, "u:\"\"},");
+        
     return FALSE;
 }
 
@@ -353,16 +449,147 @@ httpd_put_config(struct evhttp_request *req, void *args)
 }
 
 void
+httpd_put_html_start(struct evbuffer *buf, char *title)
+{
+    evbuffer_add_printf(buf, "<head><title>Thrashd - %s</title><script src=\"http://yui.yahooapis.com/3.4.1/build/yui/yui-min.js\"></script></head>", title);
+    evbuffer_add_printf(buf, "<body>");
+    evbuffer_add_printf(buf, "<a href='/connections.html'>Connections</a>&nbsp;");
+    evbuffer_add_printf(buf, "<a href='/holddowns.html'>Holddowns</a>&nbsp;");
+    evbuffer_add_printf(buf, "<a href='/addrs.html'>Addresses</a>&nbsp;");
+    evbuffer_add_printf(buf, "<a href='/hosts.html'>Hosts</a>&nbsp;");
+    evbuffer_add_printf(buf, "<a href='/uris.html'>URIs</a>&nbsp;");
+    evbuffer_add_printf(buf, "<hr><div id=\"table\"></div>");
+    evbuffer_add_printf(buf, "<script type=\"text/javascript\">");
+    evbuffer_add_printf(buf, "YUI().use(\"datatable-sort\", function (Y) {");
+}
+
+void
+httpd_put_html_end(struct evbuffer *buf, char *summary)
+{
+    evbuffer_add_printf(buf, "], dt1 = new Y.DataTable.Base({columnset:cols, recordset:data, summary:\"%s\"}).plug(Y.Plugin.DataTableSort).render(\"#table\"); });", summary);
+    evbuffer_add_printf(buf, "</script></body>");
+}
+
+void
+httpd_put_holddowns_html (struct evhttp_request *req, void *arg)
+{
+    struct evbuffer *buf;
+
+    buf = evbuffer_new();
+    httpd_put_html_start(buf, "Hold downs");
+    evbuffer_add_printf(buf, "var cols = [");
+    evbuffer_add_printf(buf, "  {key:\"ip\", label:\"Blocked IP\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"conn\", label:\"Triggered By\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"c\", label:\"Count\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"s\", label:\"Soft (s)\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"h\", label:\"Hard (s)\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"r\", label:\"Recent (s)\", sortable:true}");
+    evbuffer_add_printf(buf, "], data = [");
+    g_tree_foreach(current_blocks, (GTraverseFunc) fill_http_blocks_html, buf);
+    httpd_put_html_end(buf, "Hold downs");
+
+    evhttp_add_header(req->output_headers, "Content-Type", "text/html");
+    evhttp_send_reply(req, HTTP_OK, "OK", buf);
+    evbuffer_free(buf);
+}
+
+
+
+void
+httpd_put_connections_html (struct evhttp_request *req, void *arg)
+{
+    struct evbuffer *buf;
+
+    buf = evbuffer_new();
+    httpd_put_html_start(buf, "Connections");
+    evbuffer_add_printf(buf, "var cols = [");
+    evbuffer_add_printf(buf, "  {key:\"ip\", label:\"Address\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"p\", label:\"Port\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"r\", label:\"Requests\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"cd\", label:\"Connection Date\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"ld\", label:\"Last Date\", sortable:true},");
+    evbuffer_add_printf(buf, "], data = [");
+    g_slist_foreach(current_connections, (GFunc) fill_current_connections_html, buf);
+    httpd_put_html_end(buf, "Connections");
+
+    evhttp_add_header(req->output_headers, "Content-Type", "text/html");
+    evhttp_send_reply(req, HTTP_OK, "OK", buf);
+    evbuffer_free(buf);
+}
+
+void
+httpd_put_addrs_html (struct evhttp_request *req, void *arg)
+{
+    struct evbuffer *buf;
+
+    buf = evbuffer_new();
+    httpd_put_html_start(buf, "Addresses");
+    evbuffer_add_printf(buf, "var cols = [");
+    evbuffer_add_printf(buf, "  {key:\"a\", label:\"Address\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"c\", label:\"Connections\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"t\", label:\"Timeout\", sortable:true},");
+    evbuffer_add_printf(buf, "], data = [");
+    g_hash_table_foreach(addr_table, (GHFunc) fill_http_addr_html, buf);
+    httpd_put_html_end(buf, "Addresses");
+
+    evhttp_add_header(req->output_headers, "Content-Type", "text/html");
+    evhttp_send_reply(req, HTTP_OK, "OK", buf);
+    evbuffer_free(buf);
+}
+
+void
+httpd_put_hosts_html (struct evhttp_request *req, void *arg)
+{
+    struct evbuffer *buf;
+
+    buf = evbuffer_new();
+    httpd_put_html_start(buf, "Hosts");
+    evbuffer_add_printf(buf, "var cols = [");
+    evbuffer_add_printf(buf, "  {key:\"ip\", label:\"Address\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"c\", label:\"Connections\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"t\", label:\"Timeout\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"u\", label:\"Host (40 char max)\", sortable:true},");
+    evbuffer_add_printf(buf, "], data = [");
+    g_hash_table_foreach(uri_table, (GHFunc) fill_http_urihost_html, buf);
+    httpd_put_html_end(buf, "Hosts");
+
+    evhttp_add_header(req->output_headers, "Content-Type", "text/html");
+    evhttp_send_reply(req, HTTP_OK, "OK", buf);
+    evbuffer_free(buf);
+}
+
+void
+httpd_put_uris_html (struct evhttp_request *req, void *arg)
+{
+    struct evbuffer *buf;
+
+    buf = evbuffer_new();
+    httpd_put_html_start(buf, "URIs");
+    evbuffer_add_printf(buf, "var cols = [");
+    evbuffer_add_printf(buf, "  {key:\"ip\", label:\"Address\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"c\", label:\"Connections\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"t\", label:\"Timeout\", sortable:true},");
+    evbuffer_add_printf(buf, "  {key:\"u\", label:\"URI (40 char max)\", sortable:true},");
+    evbuffer_add_printf(buf, "], data = [");
+    g_hash_table_foreach(uri_table, (GHFunc) fill_http_urihost_html, buf);
+    httpd_put_html_end(buf, "URIs");
+
+    evhttp_add_header(req->output_headers, "Content-Type", "text/html");
+    evhttp_send_reply(req, HTTP_OK, "OK", buf);
+    evbuffer_free(buf);
+}
+
+void
 httpd_driver(struct evhttp_request *req, void *arg)
 {
     struct evbuffer *buf;
 
     buf = evbuffer_new();
 
+    evhttp_add_header(req->output_headers, "Content-Type", "text/plain");
     evbuffer_add_printf(buf, "Thrashd version: %s [%s]\n", VERSION,
                         process_name);
 
-    evhttp_add_header(req->output_headers, "Content-Type", "text/plain");
     evhttp_send_reply(req, HTTP_OK, "OK", buf);
     evbuffer_free(buf);
 }
@@ -377,11 +604,16 @@ webserver_init(void)
         return -1;
 
     evhttp_set_cb(httpd, "/holddowns", httpd_put_holddowns, NULL);
+    evhttp_set_cb(httpd, "/holddowns.html", httpd_put_holddowns_html, NULL);
     evhttp_set_cb(httpd, "/config", httpd_put_config, NULL);
     evhttp_set_cb(httpd, "/connections", httpd_put_connections, NULL);
+    evhttp_set_cb(httpd, "/connections.html", httpd_put_connections_html, NULL);
     evhttp_set_cb(httpd, "/addrs", httpd_put_addrs, NULL);
+    evhttp_set_cb(httpd, "/addrs.html", httpd_put_addrs_html, NULL);
     evhttp_set_cb(httpd, "/uris", httpd_put_uris, NULL);
+    evhttp_set_cb(httpd, "/uris.html", httpd_put_uris_html, NULL);
     evhttp_set_cb(httpd, "/hosts", httpd_put_hosts, NULL);
+    evhttp_set_cb(httpd, "/hosts.html", httpd_put_hosts_html, NULL);
     evhttp_set_gencb(httpd, httpd_driver, NULL);
     return 0;
 }
